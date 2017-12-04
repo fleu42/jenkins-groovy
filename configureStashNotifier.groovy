0ac71b8730bf15749dc3295d75f9e9434cbea47c
fleu42 <fleu42@gmail.com>
2017-12-04 15:36:21 +0100

    Create configureStashNotifier.groovy

---
+++ configureStashNotifier.groovy
@@ -0,0 +1,26 @@
+#!groovy
+import jenkins.model.*;
+import org.jenkinsci.plugins.*;
+import net.sf.json.JSONObject;
+
+String url = 'https://stashblablablabla';
+String credentials = '01111111-e222-3333-eeff-4f4444e44bc4';
+
+def j = Jenkins.getInstance();
+
+def stash = j.getExtensionList(
+  org.jenkinsci.plugins.stashNotifier.StashNotifier.DescriptorImpl.class
+)[0];
+
+def formData = [
+  stashRootUrl: url,
+  credentialsId: credentials,
+  ignoreUnverifiedSsl: false,
+  includeBuildNumberInKey: false,
+  prependParentProjectKey: false,
+  disableInprogressNotification: false,
+  considerUnstableAsSuccess: false
+] as JSONObject;
+
+stash.configure(null, formData);
+j.save();
